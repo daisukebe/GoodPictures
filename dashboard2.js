bc001e6b5855137f35b6f23f132e1a4033192423
daisukebe <poleon.kd@gmail.com>
2011-06-12 14:02:57 +0900

    first commit

--- dashboard2.js
+++ dashboard2.js
@@ -3,7 +3,6 @@ mainWin = Ti.UI.currentWindow;
 //Ti.include('findtags.js');
 
 var url = "http://www.tumblr.com/tagged/" + "fashion";
-//var url = "http://www.tumblr.com/api/dashboard/json?debug=1&start=0&email=poleon.kd@gmail.com&password=28160tumblr&tagged=fashion";
 
 var loader = Titanium.Network.createHTTPClient();
 loader.open('GET', url);
